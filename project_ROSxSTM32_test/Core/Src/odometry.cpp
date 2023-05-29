96cf37170f3977814a341e96f6cd0865cd3eed2e
weifu_yee <chenweifu9999@gmail.com>
2023-05-30 01:08:45 +0800

    先存先存

--- project_ROSxSTM32_test/Core/Src/odometry.cpp
+++ project_ROSxSTM32_test/Core/Src/odometry.cpp
@@ -10,29 +10,15 @@ void odom_update(double Vx_world, double Vy_world, double W){
     while(odom.theta < -1*PI) odom.theta += 2*PI;
     return;
 }
-//double vel_World2Car(char coor, double Vx_world, double Vy_world){
-//	double coff = 1/ cos(2 * odom.theta);
-//	if(coor == 'x')
-//		return coff * (Vy_world * cos(odom.theta) - Vx_world * sin(odom.theta));
-//	else
-//		return coff * (Vx_world * cos(odom.theta) - Vy_world * sin(odom.theta));
-//}
 double vel_World2Car(char coor, double Vx_world, double Vy_world){
 	if(coor == 'x')
 		return Vx_world * sin( odom.theta ) - Vy_world * cos( odom.theta );
 	else
 		return Vx_world * cos( odom.theta ) + Vy_world * sin( odom.theta );
 }
-//double vel_Car2World(char coor, double Vx, double Vy){
-//	if(coor == 'x')
-//		return Vx * sin( odom.theta ) + Vy * cos( odom.theta );
-//	else
-//		return Vx * cos( odom.theta ) + Vy * sin( odom.theta );
-//}
 double vel_Car2World(char coor, double Vx, double Vy){
 	if(coor == 'x')
 		return Vx * sin( odom.theta ) + Vy * cos( odom.theta );
 	else
 		return Vy * sin( odom.theta ) - Vx * cos( odom.theta );
 }
-
